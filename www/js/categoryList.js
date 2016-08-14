f6d0412cdf691c3b894500a52a446c1323d63ca2
phuchieuct <phuchieuct93@gmail.com>
2016-08-14 22:57:36 +0700

    Create new project

--- www/js/categoryList.js
+++ www/js/categoryList.js
@@ -1,47 +1,53 @@
 angular.module('starter.controllers')
-        .controller('CategoryLisrCtrl', function (CategoryService, $scope, $stateParams, $timeout,cache,$ionicScrollDelegate) {
-          var headerHeight=0;
+  .controller('CategoryLisrCtrl', function (CategoryService, $scope, $stateParams, $timeout, cache, $ionicScrollDelegate) {
+    var headerHeight = 0;
 
-          $scope.scrollTo = function(position){
+    $scope.scrollTo = function (position) {
 
-            var delegate = $ionicScrollDelegate.$getByHandle('categoryList');
-            delegate.scrollTo(0,(420*position)+headerHeight,false);
+      var delegate = $ionicScrollDelegate.$getByHandle('categoryList');
+      delegate.scrollTo(0, (420 * position) + headerHeight, false);
 
-          }
-            $scope.title = $stateParams.categoryTitle;
-            $scope.categoryId = $stateParams.categoryId;
-            $scope.articlelist = CategoryService.getArticleList();
+    }
+    $scope.title = $stateParams.categoryTitle;
+    $scope.categoryId = $stateParams.categoryId;
+    $scope.articlelist=[];
+    CategoryService.clean();
 
-            $scope.loadMore = function () {
-                CategoryService.loadMore(function (data) {
-                    $scope.articlelist = $scope.articlelist.concat(data)
-                    $scope.$broadcast('scroll.infiniteScrollComplete');
-                    $scope.$broadcast('scroll.refreshComplete');
-
-
-                })
-
+    $scope.loadMore = function () {
+        $timeout(function(){
+          CategoryService.loadMore(function (data) {
+            $scope.articlelist = $scope.articlelist.concat(data)
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+            $scope.$broadcast('scroll.refreshComplete');
 
 
-            };
-            $scope.doRefresh = function () {
-                CategoryService.clean();
-
-                $scope.articlelist = []
-                $timeout($scope.loadMore,500);
-
-            };
-
-          $scope.$on("$ionicView.beforeEnter", function(event, data){
-            // handle event
-            $scope.articlelist = CategoryService.getArticleList();
-
-            var position = cache.get("selected-feed");
-            if(position!=undefined){
-              $scope.scrollTo(cache.get("selected-feed"))
-
-            }
-          });
+          })
+        },500)
 
 
-        })
+
+
+
+    };
+    $scope.doRefresh = function () {
+      CategoryService.clean();
+      console.log("refresh")
+      $scope.articlelist = []
+      $timeout($scope.loadMore, 500);
+
+    };
+
+
+    $scope.$on("$ionicView.beforeEnter", function (event, data) {
+      // handle event
+
+      var position = cache.get("selected-feed");
+      if (position != undefined) {
+        $scope.scrollTo(cache.get("selected-feed"))
+
+      }
+    });
+
+
+
+  })
